9646dc841ffba15a6ff8bcf5dcd8528b1650a91e
Allan Kildare <allankildare27@gmail.com>
2021-03-09 04:24:33 -0300

    ~ alteracao de estilo nos componentes Header e Footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,16 +4,14 @@ import { Link } from 'react-router-dom'
 
 function Header() {
     return (
-        <header className="Menu" style={{fontSize: '24px', fontWeight: 'bold'}}>
-            <nav className="NavMenu">
+        <header className="Menu">
+            <nav className="navMenu">
+                <h1 className="HeaderLogo">
+                    <Link to="/">Bons Drinks</Link>
+                </h1>
                 <ul>
                     <li>Drinks</li>
                     <li>Sobre nós</li>
-                    <li>
-                        <h1 className="HeaderLogo">
-                            <Link to="/">Bons Drinks</Link>
-                        </h1>
-                    </li>
                     <li>Nosso time</li>
                     <li>
                         <Link to="/contato">Contato</Link>
