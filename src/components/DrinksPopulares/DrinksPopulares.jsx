57ba9032cb5e03f3335cf0279d25a951036aa477
Allan Kildare <allankildare27@gmail.com>
2021-03-28 05:14:40 -0300

    arquivo css desnecessario deletado

--- src/components/DrinksPopulares/DrinksPopulares.jsx
+++ src/components/DrinksPopulares/DrinksPopulares.jsx
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react'
-import style from './DrinksPopulares.module.css'
 import styled from 'styled-components'
 import Loading from '../Loading/Loading'
 
